571c1d729d75318e991964c4bd212126d8630d52
Pollox <polloxreg@gmail.com>
2018-11-07 20:38:48 -0500

    added scrollbar
    redid the list of quests as a ZO_tree inside a ZO_ScrollContainer

--- DailyQuestTracker.lua
+++ DailyQuestTracker.lua
@@ -1,11 +1,8 @@
 DailyQuestTracker = {}
 
 DailyQuestTracker.name = "PolloxsDailyQuestTracker"
-
---[[
-	Section Name -> {questTypeName -> DQTQuestRow control}
---]]
-DailyQuestTracker.rowControlInfos = {}
+DailyQuestTracker.checkedTexture = "/esoui/art/buttons/checkbox_checked.dds"
+DailyQuestTracker.uncheckedTexture = "esoui/art/buttons/checkbox_unchecked.dds"
 
 -- TODO: can we implement as a set instead?
 local function tableContains(t, v)
@@ -18,6 +15,11 @@ local function tableContains(t, v)
 	return false
 end
 
+-- get current time in UTC seconds
+local function getCurrentTime()
+	return os.time(os.date("!*t"))
+end
+
 --[[
 	@param eventCode (number)
 	@param journalIndex (number)
@@ -27,10 +29,10 @@ end
 function DailyQuestTracker.onQuestAdded(eventCode, journalIndex, questName, objectiveName)
 	-- if this is one of the daily quests we track, then update the quest status
 	if tableContains(L, questName) then
-		-- utc time
-		local currentTime = os.time(os.date("!*t"))
-		
-		DailyQuestTracker.questStatuses[questName] = {addedTime = currentTime, isCompleted = false}
+		DailyQuestTracker.questStatuses[questName] = {
+			addedTime = getCurrentTime(),
+			isCompleted = false
+		}
 	end
 end
 
@@ -53,57 +55,6 @@ function DailyQuestTracker.onQuestComplete(eventCode, questName, level, previous
 	end
 end
 
--- Create rows in window
-function DailyQuestTracker:createRows()
-	local previousSection
-	
-	-- Create a section for each category of quests
-	for sectionIndex, section in ipairs(DQTInfo.Quests) do
-		local sectionControl = CreateControlFromVirtual("QuestSection", DQTWindowDummyFrame, "DQTQuestSection", sectionIndex)
-		sectionControl:GetNamedChild("Name"):SetText(section.name)
-		
-		if previousSection then
-			sectionControl:SetAnchor(TOP, previousSection, BOTTOM, 0, 0)
-		end
-		
-		-- Align the first row relative to the section heading, and subsequent rows relative to the previous row
-		local previousRow = sectionControl:GetNamedChild("Name")
-		
-		local rowControlInfo = {}
-		local numRows = 0
-		
-		-- Create a row for each quest in this section
-		for questTypeName, _ in pairs(section.quests) do
-			local rowControl = CreateControlFromVirtual("QuestRow"..sectionIndex.."_", sectionControl, "DQTQuestRow", questTypeName)
-			rowControl:GetNamedChild("Name"):SetText(questTypeName)
-			
-			if previousRow then
-				rowControl:SetAnchor(TOP, previousRow, BOTTOM, 0, 0)
-			end
-			
-			rowControlInfo[questTypeName] = rowControl
-			
-			previousRow = rowControl
-			numRows = numRows + 1
-		end
-		
-		sectionControl:SetHeight(sectionControl:GetNamedChild("Name"):GetHeight() + previousRow:GetHeight() * numRows)
-		self.rowControlInfos[section.name] = rowControlInfo
-		previousSection = sectionControl
-	end
-end
-
-function DailyQuestTracker:initialize()
-	-- Register keybindings
-	ZO_CreateStringId("SI_BINDING_NAME_DTQ_TOGGLE_DISPLAY", "Toggle Display")
-	self:createRows()
-	
-	self.SavedVariables = ZO_SavedVars:New("PolloxsDailyQuestTracker_SavedVariables", 3, nil, {questStatuses = {}})
-	
-	-- Quest Name -> (time added (in utc), completed (boolean))
-	self.questStatuses = self.SavedVariables.questStatuses
-end
-
 --[[
 	Is the given daily quest complete today? For example, for a Summerset World Boss,
 	this will check if any of the Summerset World Boss quests were completed today.
@@ -111,8 +62,8 @@ end
 	@param resetTime (integer) UTC time in seconds
 	@param questNames (list of strings) all the possible quest names for this quest
 --]]
-function DailyQuestTracker:isDailyQuestComplete(resetTime, questNames)
-	local previousResetTime = resetTime - 86400
+function DailyQuestTracker:isDailyQuestComplete(questNames)
+	local previousResetTime = self.resetTime - 86400
 	
 	for _, questName in ipairs(questNames) do
 		questStatus = self.questStatuses[questName]
@@ -128,10 +79,10 @@ function DailyQuestTracker:isDailyQuestComplete(resetTime, questNames)
 	return false
 end
 
-function DailyQuestTracker:update()
-	--[[ Get time of next reset in UTC and time until reset.
-	     This uses your computer's time. If your computer clock is off, this will be off.
-	--]]
+--[[ Calculates time of next daily quest reset in UTC.
+     If your computer clock is off, this will be off.
+--]]
+function DailyQuestTracker.getResetTime()
 	local currentDate = os.date("!*t")
 	local resetTime = os.time({year=currentDate.year, month=currentDate.month, day=currentDate.day, hour=6, minute=0, second=0, isdst=currentDate.isdst})
 	local currentTime = os.time(currentDate)
@@ -141,7 +92,19 @@ function DailyQuestTracker:update()
 		resetTime = resetTime + 86400
 	end
 	
-	local timeUntilReset = os.difftime(resetTime, currentTime)
+	return resetTime
+end
+
+function DailyQuestTracker:update()
+	local currentTime = getCurrentTime()
+	
+	-- calculate next reset time if necessary
+	if self.resetTime < currentTime then
+		self.resetTime = self.getResetTime()
+		self:createRows()
+	end
+	
+	local timeUntilReset = os.difftime(self.resetTime, currentTime)
 	
 	-- format and display the remaining time
 	local hoursRemaining = math.floor(timeUntilReset / 3600)
@@ -149,27 +112,6 @@ function DailyQuestTracker:update()
 	-- round minutes up since we're not displaying seconds
 	local minutesRemaining = math.ceil((timeUntilReset - hoursRemaining * 3600) / 60)
     DQTWindowTimeUntilReset:SetText(string.format("Time until reset: %d hours %d minutes", hoursRemaining, minutesRemaining))
-	
-	-- Update status icons
-	local checkedTexture = "/esoui/art/buttons/checkbox_checked.dds"
-	local uncheckedTexture = "esoui/art/buttons/checkbox_unchecked.dds"
-	
-	for _, section in ipairs(DQTInfo.Quests) do
-		local rowControlInfo = self.rowControlInfos[section.name]
-		
-		for questTypeName, questNames in pairs(section.quests) do
-			local rowControl = rowControlInfo[questTypeName]
-			local statusTexture;
-			
-			if self:isDailyQuestComplete(resetTime, questNames) then
-				statusTexture = checkedTexture
-			else
-				statusTexture = uncheckedTexture
-			end
-			
-			rowControl:GetNamedChild("Status"):SetTexture(statusTexture)
-		end
-	end
 end
 
 -- Show or hide the window
@@ -182,9 +124,57 @@ function DailyQuestTracker:closeWindow()
 	DQTWindow:SetHidden(true)
 end
 
+-- data {name: section name}
+local function TreeSectionSetup(node, control, data, open, userRequested, enabled)
+	control:GetNamedChild("Name"):SetText(data.name)
+end
+
+-- data {name: quest type name, isCompletedToday: (boolean)}
+local function TreeQuestTypeSetup(node, control, data, open, userRequested, enabled)
+	control:GetNamedChild("Name"):SetText(data.name)
+	
+	local statusTexture = data.isCompletedToday and DailyQuestTracker.checkedTexture or DailyQuestTracker.uncheckedTexture	
+	control:GetNamedChild("Status"):SetTexture(statusTexture)
+end
+
+function DailyQuestTracker:createRows()
+	-- Create a section for each category of quests
+	for _, section in ipairs(DQTInfo.Quests) do
+		local sectionNode = self.tree:AddNode("DQTQuestSection", {name = section.name})
+		
+		-- Create a row for each quest in this section
+		for questTypeName, questNames in pairs(section.quests) do
+			local questTypeData = {name = questTypeName,
+				isCompletedToday = self:isDailyQuestComplete(questNames)}
+			self.tree:AddNode("DQTQuestType", questTypeData, sectionNode)
+		end
+		
+		sectionNode:SetOpen(true)
+	end
+end
+
+function DailyQuestTracker:initialize()
+	-- Register keybindings
+	ZO_CreateStringId("SI_BINDING_NAME_DTQ_TOGGLE_DISPLAY", "Toggle Display")
+	
+	self.SavedVariables = ZO_SavedVars:New("PolloxsDailyQuestTracker_SavedVariables", 3, nil, {questStatuses = {}})
+	
+	-- Quest Name -> (time added (in utc), completed (boolean))
+	self.questStatuses = self.SavedVariables.questStatuses
+	
+	self.resetTime = self.getResetTime()
+	
+	local scrollContainer = DQTWindow:GetNamedChild("ScrollFrame")
+	self.tree = ZO_Tree:New(scrollContainer:GetNamedChild("ScrollChild"), 0, 0, 500)
+	self.tree:AddTemplate("DQTQuestSection", TreeSectionSetup, nil, nil, 0, 0)
+	self.tree:AddTemplate("DQTQuestType", TreeQuestTypeSetup, nil, nil, 0, 0)
+	self:createRows()
+end
+
 function DailyQuestTracker.OnAddOnLoaded(event, addonName)
 	-- The event fires each time *any* addon loads - but we only care about when our own addon loads
 	if addonName == DailyQuestTracker.name then
+		EVENT_MANAGER:UnregisterForEvent(DailyQuestTracker.name, EVENT_ADD_ON_LOADED)
 		DailyQuestTracker:initialize()
 	end
 end
